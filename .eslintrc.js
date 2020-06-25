615ccf533f62eb8563b2f5726e9951e50c4e5dfc
Julien Wajsberg <felash@gmail.com>
2020-06-25 22:44:25 +0200

    Convert to typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,18 @@ module.exports = {
     es6: true,
     node: true,
   },
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    'plugin:flowtype/recommended',
     'prettier',
-    'prettier/flowtype',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
   parserOptions: {
-    ecmaVersion: '2017',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-    },
-    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-  plugins: ['babel', 'flowtype', 'import', 'prettier'],
+  plugins: ['babel', 'import', 'prettier', '@typescript-eslint'],
   rules: {
     // Plugin rules:
     'import/no-duplicates': 'error',
@@ -25,11 +22,6 @@ module.exports = {
     'import/no-default-export': 'error',
     'import/named': 'error',
     'prettier/prettier': 'error',
-    'flowtype/require-valid-file-annotation': [ 'error', 'always', { annotationStyle: 'line' } ],
-    // no-dupe-keys crashes with recent eslint. See
-    // https://github.com/gajus/eslint-plugin-flowtype/pull/266 and
-    // https://github.com/gajus/eslint-plugin-flowtype/pull/302
-    // 'flowtype/no-dupe-keys': 'error',
 
     // overriding recommended rules
     'no-constant-condition': ['error', { checkLoops: false }],
@@ -43,7 +35,7 @@ module.exports = {
     'array-callback-return': 'error',
     'consistent-return': 'error',
     'default-case': 'error',
-    'dot-notation': 'error',
+    '@typescript-eslint/dot-notation': 'error',
     eqeqeq: 'error',
     'for-direction': 'error',
     'no-alert': 'error',
@@ -53,20 +45,16 @@ module.exports = {
     'no-extra-bind': 'error',
     'no-extra-label': 'error',
     'no-implied-eval': 'error',
-    // We use the version from the babel plugin so that `this` in a function
-    // class property doesn't give a false positive.
-    'babel/no-invalid-this': 'error',
+    '@typescript-eslint/no-invalid-this': 'error',
     'no-return-await': 'error',
     'no-self-compare': 'error',
     'no-throw-literal': 'error',
     'no-unmodified-loop-condition': 'error',
-    // We use the version from the flowtype plugin so that flow assertions don't
-    // output an error.
-    'flowtype/no-unused-expressions': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
     'no-useless-call': 'error',
     'no-useless-computed-key': 'error',
     'no-useless-concat': 'error',
-    'no-useless-constructor': 'error',
+    '@typescript-eslint/no-useless-constructor': 'error',
     'no-useless-rename': 'error',
     'no-useless-return': 'error',
     'no-var': 'error',
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,7 @@
-"use strict";
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-module.exports = {
-    collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**', '!src/types/libdef/**'],
-    setupFilesAfterEnv: ['./test/setup.js'],
-    verbose: false
-};
+
+// This file just reexports all middlewares for an easier access.
+export * from './versioning';
+export * from './reportTo';
